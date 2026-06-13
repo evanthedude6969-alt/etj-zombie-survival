145493e9c3979a18fd7d4f2a65f8b918e14e4aaa
evanthedude6969-alt <evanthedude6969@gmail.com>
2026-06-13 00:47:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,19 +12,44 @@ const player = {
 };
 
 const keys = {};
+const bullets = [];
+let mouseX = 0;
+let mouseY = 0;
 
 document.addEventListener("keydown", e => keys[e.key.toLowerCase()] = true);
 document.addEventListener("keyup", e => keys[e.key.toLowerCase()] = false);
+
+canvas.addEventListener("mousemove", e => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+});
+
+canvas.addEventListener("click", () => {
+    const angle = Math.atan2(mouseY - player.y, mouseX - player.x);
+
+    bullets.push({
+        x: player.x,
+        y: player.y,
+        dx: Math.cos(angle) * 10,
+        dy: Math.sin(angle) * 10,
+        size: 6
+    });
+});
 
 function update() {
     if(keys["w"]) player.y -= player.speed;
     if(keys["s"]) player.y += player.speed;
     if(keys["a"]) player.x -= player.speed;
     if(keys["d"]) player.x += player.speed;
+
+    bullets.forEach(bullet => {
+        bullet.x += bullet.dx;
+        bullet.y += bullet.dy;
+    });
 }
 
 function draw() {
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     ctx.fillStyle = "lime";
     ctx.fillRect(
@@ -33,6 +58,13 @@ function draw() {
         player.size,
         player.size
     );
+
+    ctx.fillStyle = "yellow";
+    bullets.forEach(bullet => {
+        ctx.beginPath();
+        ctx.arc(bullet.x, bullet.y, bullet.size, 0, Math.PI * 2);
+        ctx.fill();
+    });
 }
 
 function gameLoop() {
